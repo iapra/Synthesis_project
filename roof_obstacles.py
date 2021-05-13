87f7738795546105541f0f67b35a00e5e896e21c
Irène Apra <ireneapra@gmail.com>
2021-05-13 18:41:23 +0200

    points subset over triangles

--- roof_obstacles.py
+++ roof_obstacles.py
@@ -10,14 +10,52 @@ import scipy.spatial
 # kd = scipy.spatial.KDTree(list_pts)
 # d, i = kd.query(p, k=1)
 
+def area(p1,p2,p3):
+    return abs((p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1])
+                + p3[0] * (p1[1] - p2[1])) / 2.0)
+
+def isInside(p1,p2,p3, p):
+# A function to check whether point P(x, y)
+# lies inside the triangle formed by
+# A(x1, y1), B(x2, y2) and C(x3, y3)
+    A = area (p1,p2,p3)
+    A1 = area (p, p2, p3)
+    A2 = area (p1, p, p3)   
+    A3 = area (p1, p2, p)
+    if(A == A1 + A2 + A3):
+        return True
+    else:
+        return False
+
+
 def detect_obstacles(point_cloud, vertices, faces, output_file):
     print(len(vertices))
     print(len(faces))
+
+    kd_pc = scipy.spatial.KDTree(point_cloud)
     # Loop through triangles and select points above it (in a local subset)
+    k = 0
     for triangle in faces:
         assert (len(triangle) == 3)
-    return
-    # Distance points to surface: discard points closer than .. threshold to define
+
+        #inside = 0 
+        outside = 0
+        subset = []
+        # Distance points to surface: discard points closer than .. threshold to define
+        for point in point_cloud:
+            # Points' subset
+            p1 = vertices[triangle[0]-1]
+            p2 = vertices[triangle[1]-1]
+            p3 = vertices[triangle[2]-1]
+            if isInside(p1, p2, p3, point): 
+                subset.append(point)
+                #inside += 1
+            else:
+                outside += 1
+        k += 1
+        print ("For triangle number ", k, " Number of point in = ", len(subset), " Number of poiunts out = ", outside)
+    
+   
 
     # Obstacle points convex-hull
 
@@ -28,5 +66,6 @@ def detect_obstacles(point_cloud, vertices, faces, output_file):
     # Store new attribute per triangle
 
 
+    return
 
 
