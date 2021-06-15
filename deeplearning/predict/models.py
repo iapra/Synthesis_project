95116b4475f10aabc3746fd52172548a3f5d8e45
Ondrej Vesely <ondrej-vesely@seznam.cz>
2021-06-15 13:16:21 +0200

    updated pretrained pv detection model

--- deeplearning/predict/models.py
+++ deeplearning/predict/models.py
@@ -3,4 +3,4 @@ import os
 
 _dir = os.path.dirname(os.path.abspath(__file__))
 
-rgb = keras.models.load_model(os.path.join(_dir, "models", "./run3_rgb_19_256/"))
+rgb = keras.models.load_model(os.path.join(_dir, "models", "./run4_rgb_19_512-to-256/"))
